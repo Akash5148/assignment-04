561f8ac79d1494eeaa22718b987cb44fbf357d7d
Akash5148 <mdsalmanahmmedakash@gmail.com>
2026-02-24 04:14:36 +0600

    complete header and main section

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let totalCount =document.getElementById('totalCount');
+
+let InterviewCount =document.getElementById('InterviewCount');
+let RejectedCount =document.getElementById('RejectedCount');
